225db1f03c565e388b2830e953ac7d655eaaa08c
Nipun Garg <77457267+ngarg2k2@users.noreply.github.com>
2021-10-19 05:52:15 +0530

    Added more functions

--- stack.cpp
+++ stack.cpp
@@ -1,53 +1,91 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-class node
-{
- public:
- int data;
- node* link;
+class node{
+    int data;
+    node *top=NULL;
+    public:
+    node *next;
+    void push(int x){
+        node *newnode=new node;
+        if(newnode==NULL){
+            cout<<"Stack is full"<<endl;
+        }
+        else{
+            newnode->data=x;
+            newnode->next=top;
+            top=newnode;
+        }
+    }
+    void display(){
+        node *temp=top;
+        while(temp){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+    int pop(){
+        int x=-1;
+        if(top==NULL){
+            cout<<"Empty"<<endl;
+        }
+        else{
+            node *temp=top;
+            top=top->next;
+            x=temp->data;
+            delete temp;
+        }
+        return x;
+    }
+    int peek(int pos){
+        int x=-1;
+        node *temp=top;
+        for (int i = 0; top && i< pos-1; i++)
+        {
+            temp=temp->next;
+        }
+        if(temp)
+            x=temp->data;
+        return x;     
+    }
+    int stackTop(){
+        if(top){
+            return top->data;
+        }
+        else{
+            return -1;
+        }
+    }
+    int isEmpty(){
+        if(top){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+    int isFull(){
+        node *t=new node;
+        if(t){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
 };
-void pop(node**head)
-{
- *head=(*head)->link;
-}
-void push(node **head,int info)
-{
- node*ref=*head;
- node*temp=new node();
- temp->data=info;
- 
- if(*head==NULL)
- {
-  temp->link= NULL;
-  *head=temp;
-  return;
- }
- else if(head!=NULL)
- {
-  temp->link=*head;
-  *head=temp;
-  return;
- }
-}
-void output(node *head)
-{
- node*ref=head;
- while(ref!=NULL)
- {
-  cout<<ref->data;
-  ref=ref->link;
-  
- }
-}
-
-int main() {
- node*head=NULL;
- push(&head,5);
- push(&head,4);
- push(&head,3);
- push(&head,2);
- output(head);
- pop(&head);
-  output(head);
-	return 0;
+int main(){
+    node n;
+    n.push(10);
+    // n.push(20);
+    n.push(20);
+    n.push(30);
+    n.push(40);
+    n.push(50);
+    n.display();
+    cout<<endl;
+    cout<<"Value of top element is "<<n.stackTop()<<endl;
+    cout<<"Pop "<<n.pop()<<endl;
+    cout<<"Value at index 2 is "<<n.peek(2)<<endl;
+    cout<<"Is empty "<<n.isEmpty()<<endl;
+    cout<<"Is full "<<n.isFull()<<endl;
 }
